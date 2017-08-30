8d92bbc1aab12b4cb4f4618788d7d52def08839c
Robert Roskam <raiderrobert@gmail.com>
2017-08-30 06:51:06 -0400

    Create __init__.py

---
+++ sbt/__init__.py
@@ -0,0 +1,11 @@
+"""
+A standalone partial implementation of sbt
+https://www.solutionsbytext.com/api-support/api-documentation/
+"""
+
+from .resources import RequireVBT, RequestVBT, Carrier, SendTemplateMessage, GetMessageStatus
+
+
+__all__ = [
+    'RequireVBT', 'SendTemplateMessage', 'Carrier', 'GetMessageStatus', 'RequestVBT'
+]
